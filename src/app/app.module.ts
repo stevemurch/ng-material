f8521385c14d108f68b5038615b840013601f627
Steve Murch <ceo@bigoven.com>
2018-03-21 09:54:30 -0700

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http'
+import { MaterialModule } from './material.module';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { SigninComponent } from './signin/signin.component';
+
+import {
+  SocialLoginModule,
+  AuthServiceConfig,
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+} from "angular5-social-login";
+
+// Configs 
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+      [
+        {
+          id: FacebookLoginProvider.PROVIDER_ID,
+          provider: new FacebookLoginProvider("960561807402484")
+        },
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider("500950983343-r2n274oobgivi683r09flr7iabqie0p0.apps.googleusercontent.com")
+        }
+      ]
+  );
+  return config;
+}
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SigninComponent
+],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    SocialLoginModule
+  ],
+  providers: [
+    { provide: AuthServiceConfig,
+    useFactory: getAuthServiceConfigs
+  }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
